e41e61b4e0d2b9b876046fbb653125807b18eb08
Behnam Hamidi <eslp91@hotmail.com>
2024-03-29 15:50:16 +0330

    rebuild

--- script.js
+++ script.js
@@ -1,51 +1,47 @@
 const itemForm = document.getElementById('form');
-const itemInput = document.getElementById('formInput');
+const formInput = document.getElementById('formInput');
+const filterInput = document.getElementById('filter');
 const itemList = document.getElementById('itemList');
 const clearBtn = document.getElementById('clearAll');
-const filterInput = document.getElementById('filter');
 
 function addItem(e) {
   e.preventDefault();
 
-  //   Validate input
-  const newItem = itemInput.value;
-
-  if (newItem.value === '') {
-    alert('Please add an item');
+  const newItem = formInput.value;
+  if (newItem === '') {
+    alert('You must add a task!');
     return;
   }
 
-  // Create list item
   const li = document.createElement('li');
-  li.appendChild(document.createTextNode(newItem));
   li.className = 'item';
 
-  const btnContainer = createBtnContainer('btnContainer');
-  li.appendChild(btnContainer);
+  const button = createButtonContainer('btnContainer');
+  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(button);
 
   itemList.appendChild(li);
+  formInput.value = '';
 
   checkUI();
-
-  itemInput.value = '';
 }
 
-// Create Btn Container
-function createBtnContainer(classes) {
-  const btnContainer = document.createElement('div');
-  btnContainer.className = classes;
+// Button Container
+function createButtonContainer(classes) {
+  const div = document.createElement('div');
+  div.className = classes;
 
-  const deleteBtn = createDeleteBtn('deleteBtn');
-  const editBtn = createEditBtn('editBtn');
+  const editButton = createEditButton('editBtn');
+  const deleteButton = createDeleteButton('deleteBtn');
 
-  btnContainer.appendChild(editBtn);
-  btnContainer.appendChild(deleteBtn);
+  div.appendChild(editButton);
+  div.appendChild(deleteButton);
 
-  return btnContainer;
+  return div;
 }
 
 // Create Buttons
-function createEditBtn(classes) {
+function createEditButton(classes) {
   const button = document.createElement('button');
   button.className = classes;
 
@@ -55,18 +51,17 @@ function createEditBtn(classes) {
   return button;
 }
 
-function createDeleteBtn(classes) {
+function createDeleteButton(classes) {
   const button = document.createElement('button');
   button.className = classes;
 
-  const icon = createDeleteIcon('fa fa-trash');
+  const icon = createDeleteIcon('fas fa-trash');
   button.appendChild(icon);
 
   return button;
 }
 
 // Create Icons
-
 function createEditIcon(classes) {
   const icon = document.createElement('i');
   icon.className = classes;
@@ -79,10 +74,10 @@ function createDeleteIcon(classes) {
   return icon;
 }
 
-// @ Remove Item
+// Remove item
 function removeItem(e) {
   let element = e.target;
-  while (element != null && !element.classList.contains('deleteBtn')) {
+  while (element != null && element.classList.contains('deleteBtn')) {
     element = element.parentElement;
   }
 
@@ -95,8 +90,8 @@ function removeItem(e) {
   checkUI();
 }
 
-function clearItems() {
-  if (window.confirm('Are you sure you want to clear all items?')) {
+function removeAll() {
+  if (window.confirm('Are you sure you want to delete all tasks?!')) {
     while (itemList.firstChild) {
       itemList.removeChild(itemList.firstChild);
     }
@@ -104,8 +99,19 @@ function clearItems() {
   checkUI();
 }
 
+function checkUI() {
+  const items = document.querySelectorAll('li');
+  if (items.length === 0) {
+    clearBtn.style.display = 'none';
+    filterInput.style.display = 'none';
+  } else {
+    clearBtn.style.display = 'block';
+    filterInput.style.display = 'block';
+  }
+}
+
 function filterItems(e) {
-  const items = itemList.querySelectorAll('li');
+  const items = document.querySelectorAll('li');
   const text = e.target.value.toLowerCase();
 
   items.forEach((item) => {
@@ -119,24 +125,10 @@ function filterItems(e) {
   });
 }
 
-function checkUI() {
-  const items = itemList.querySelectorAll('li');
-
-  if (items.length === 0) {
-    clearBtn.style.display = 'none';
-    filterInput.style.display = 'none';
-  } else {
-    clearBtn.style.display = 'block';
-    filterInput.style.display = 'block';
-  }
-}
-
 checkUI();
 
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
-clearBtn.addEventListener('click', clearItems);
+clearBtn.addEventListener('click', removeAll);
 filterInput.addEventListener('input', filterItems);
-
-// console.log(items.firstChild.innerHTML);
